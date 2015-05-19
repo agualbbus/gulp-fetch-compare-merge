ab34dde879019582ad88f1a12f2c7e0bcc216990
agualbbus <agualbbus>
2015-05-19 15:30:53 -0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,29 @@ var exec = require('exec-chainable');
 var gulp = require('gulp');
 var git = require('gulp-git');
 
-var head={};
-gulp.task('head', function() {
+
+gulp.task('fetch', function(){
+  git.fetch('origin', '', {args: ''}, function (err) {
+    if (err) throw err;
+  });
+});
+ 
+
+gulp.task('merge',['fetch'], function() {
+    var head={};
     exec('git rev-list master -n 1').then(function (stdout) {
         head.local=stdout;
+        console.log('local is',head.local)
+        
         exec('git rev-list origin/master -n 1').then(function(stdout){
             head.origin=stdout;
+            console.log('remote is',head.origin)
         })
         .done(function (stdout) {
               if (head.origin!==head.local){
                   git.merge('origin/master', function (err) {
                     if (err) throw err;
+                    console.log('merged')   
                   });
               }
         });
